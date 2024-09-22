bb39e052ab9c3fb54c342d0d05bf8b70a3582bef
TanKhang <doank3442@gmail.com>
2024-09-23 00:20:08 +0700

    refactor/program

--- src/container/program/topup/create-edit/topup-create-edit.tsx
+++ src/container/program/topup/create-edit/topup-create-edit.tsx
@@ -246,14 +246,13 @@ function TopupCreateEdit() {
                 : values.products.map((item) => item.value).join(",")
               : "",
 
-          // objectives:
-          //   typeof values.objectives !== "string"
-          //     ? values.objectives.length === 1 &&
-          //       values.objectives[0]?.value === "all"
-          //       ? OBJECTIVES_SELECT?.map((item) => item.value).join(",")
-          //       : values.objectives.map((item) => item.value).join(",")
-          //     : "",
-          objectives: "RETAILER2",
+          objectives:
+            typeof values.objectives !== "string"
+              ? values.objectives.length === 1 &&
+                values.objectives[0]?.value === "all"
+                ? OBJECTIVES_SELECT?.map((item) => item.value).join(",")
+                : values.objectives.map((item) => item.value).join(",")
+              : "",
           price: +(values?.price ?? 0),
           time_start:
             values.time_start &&
@@ -336,7 +335,7 @@ function TopupCreateEdit() {
           name: topupProgram?.name ?? "",
           products: mapCodeProduct(topupProgram?.products),
           price: topupProgram?.price ?? "",
-          objectives: [],
+          objectives: mapObjective(topupProgram?.objectives) ?? "",
           time_end: topupProgram?.time_end
             ? format(new Date(topupProgram.time_end), "yyyy-MM-dd")
             : ("" as any),
