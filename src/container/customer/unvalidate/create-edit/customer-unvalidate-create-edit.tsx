befda33b921603f488f42b81ad41b0369eddf236
TanKhang <doank3442@gmail.com>
2024-09-23 15:10:48 +0700

    refactor/verify-customer

--- src/container/customer/unvalidate/create-edit/customer-unvalidate-create-edit.tsx
+++ src/container/customer/unvalidate/create-edit/customer-unvalidate-create-edit.tsx
@@ -264,7 +264,15 @@ function CustomerUnValidationCreateEdit() {
                   {isEdit && isCreate !== "true" && (
                     <div className="d-flex gap-2">
                       {permission.verifyUnvalidateCustomer && (
-                        <AppWarning onAccept={() => handleSubmit()}>
+                        <AppWarning
+                          onAccept={() => {
+                            if (values.customer_type === "ANONYMOUS") {
+                              toast.showToast(
+                                "Đối tượng khách hàng phải khác chưa định danh trước khi xác thực khách hàng"
+                              );
+                            } else handleSubmit();
+                          }}
+                        >
                           <button
                             className={`btn btn-teal-light justify-content-center align-items-center ${
                               isLoadingVerify && "btn-loader "
