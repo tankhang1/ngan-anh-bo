73bb779128147c6dfaffaa61da925e895633b307
Ethan <doank3442@gmail.com>
2025-02-26 20:13:32 +0700

    refactor/baseQueryWithReauth

--- src/redux/middlewares/baseQueryWithReauth.ts
+++ src/redux/middlewares/baseQueryWithReauth.ts
@@ -1,9 +1,6 @@
 import { FetchBaseQueryError } from "@reduxjs/toolkit/query";
 import { BaseQueryFn, FetchArgs } from "@reduxjs/toolkit/query";
 import baseQuery from "./baseQuery";
-import { resetAccountInfo, resetToken } from "../slices/authSlice";
-import { HTTPS_METHOD } from "../../constants";
-import store from "../store";
 
 const baseQueryWithReauth: BaseQueryFn<
   string | FetchArgs,
@@ -14,31 +11,7 @@ const baseQueryWithReauth: BaseQueryFn<
 
   if (result.error && result.error.status === 403) {
     // Token expired or unauthorized, attempt to refresh the token
-    try {
-      const refreshResult = await baseQuery(
-        {
-          url: "/refresh-token",
-          method: HTTPS_METHOD.POST,
-        },
-        api,
-        extraOptions
-      );
-      if (refreshResult.data) {
-        // Assuming the API response contains the new token
-        const newToken = (refreshResult.data as { data: string }).data;
-
-        // Store the new token
-        api.dispatch(resetToken({ token: newToken }));
-
-        // Retry the original query with the new token
-        result = await baseQuery(args, api, extraOptions);
-      } else {
-        // Refresh failed, log out the user
-        api.dispatch(resetAccountInfo());
-      }
-    } catch (error) {
-      api.dispatch(resetAccountInfo());
-    }
+    window.location.replace(window.location.origin);
   }
 
   return result;
